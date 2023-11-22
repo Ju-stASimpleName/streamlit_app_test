0d2bf413807bad3e291837e0c6c47c321a308039
Ju_u <146108110+Ju-stASimpleName@users.noreply.github.com>
2023-11-22 09:34:20 +0100

    Add files via upload

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -8,29 +8,42 @@ st.title('Quest on Streamlit : build and share data apps!')
 
 link_cars = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 data_cars = pd.read_csv(link_cars)
-df_cars = pd.DataFrame(data_cars)
-df_cars.info()
 
-# Obtenez la liste complète des continents disponibles
-all_continents = df['continent'].unique()
+try:
+	df_cars = pd.DataFrame(data_cars)
 
-# Ajouter des boutons pour filtrer par continent avec tous les continents sélectionnés par défaut
-st.sidebar.header('Filtrer par continent')
-continents = st.sidebar.multiselect('Sélectionner les continents:', all_continents, default=all_continents)
+	# Obtenez la liste complète des continents disponibles
+	all_continents = df_cars ['continent'].unique()
 
-# Appliquer le filtre à l'ensemble du DataFrame
-filtered_df = df[df['continent'].isin(continents) if continents else True]
+	# Ajouter des boutons pour filtrer par continent avec tous les continents sélectionnés par défaut
+	st.sidebar.header('Filtrer par continent')
+	continents = st.sidebar.multiselect('Sélectionner les continents:', all_continents, default=all_continents)
 
-st.title("Here's the DataFrame!")
-df_cars
+	if not continents:
+        	st.error("Please select at least one continent.")
 
-st.title("Here's the pairplot!")
-viz_correlation = sns.heatmap(df_cars.select_dtypes("number").corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
-st.pyplot(viz_correlation.figure)
+	else:
 
-st.title("Here's the scatterplots!")
-unique_continents = df_cars['continent'].unique().tolist()
-selected_continents = st.multiselect("Choose continent", unique_continents)
+		# Appliquer le filtre à l'ensemble du DataFrame
+		filtered_df = df_cars [df_cars ['continent'].isin(continents) if continents else True]
 
-st.subheader("Scatterplot: MPG vs HP")
-st.scatter_chart(filtered_data, x="mpg", y="hp", color="continent")
+		st.title("Here's the DataFrame!")
+		df_cars
+
+		st.title("Here's the pairplot!")
+		viz_correlation = sns.heatmap(df_cars.select_dtypes("number").corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
+		st.pyplot(viz_correlation.figure)
+
+		st.title("Here's the scatterplot!")
+
+		st.subheader("Scatterplot: MPG vs HP")
+		st.scatter_chart(filtered_df, x="mpg", y="hp", color="continent")
+
+except URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+        Connection error: %s
+    """
+        % e.reason
+    )
